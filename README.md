b16faa713483c054e138d73173bc422eb163d390
Zeyad Naguib <82226885+zeyadmtn@users.noreply.github.com>
2024-07-22 17:47:12 +0300

    Initiate project

---
+++ README.md
@@ -0,0 +1,7 @@
+# React + Vite
+
+Run `npm run dev` to start the project.
+
+All assets are included in the project.
+
+Goodluck!
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-5xl m-auto'>
+        Goodluck!
+      </div>
+    </>
+  )
+}
+
+export default App
